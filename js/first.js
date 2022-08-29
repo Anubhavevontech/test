99fbb93f3edd04136a10ba28562e00dc8a2d245a
anubhav.dobhal <anubhav.dobhal@evontech.com>
2022-08-29 19:05:14 +0530

    k

--- js/first.js
+++ js/first.js
@@ -1,7 +1,8 @@
 let sceneToRender;
-const cam = document.getElementById("move").value;
 const canvas = document.getElementById("renderCanvas");
 const engine = new BABYLON.Engine(canvas, true);
+var camera1;
+var camera2;
 
 
 const createScene = async () => {
@@ -22,28 +23,71 @@ const createScene = async () => {
             newMeshes[0].scaling = new BABYLON.Vector3(4, 4, 4);
         }
     );
-    //    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2,  1, -2, new BABYLON.Vector3(0, 0, 0));
-   
+        //camera-1
+   camera1 = new BABYLON.DeviceOrientationCamera(
+    "DevOr_camera",
+    new BABYLON.Vector3(0, 0, -10),
+    scene
+  );
 
-    // camera1.attachControl(canvas, true);
-    // camera1.lowerRadiusLimit = 8
-    // camera1.upperRadiusLimit = 15
+  camera1.setTarget(new BABYLON.Vector3(0, 0, 0));
 
-    var camera = new BABYLON.DeviceOrientationCamera("DevOr_camera", new BABYLON.Vector3(0, 0, -10), scene);
+  camera1.angularSensibility = 2.5;
+  camera1.moveSensibility = 2.5;
 
-    // Targets the camera to a particular position
-    camera.setTarget(new BABYLON.Vector3(0, 0, 0));
+
+  camera1.attachControl(canvas, true);
+  camera1.upperRadiusLimit = 10;
+  camera1.lowerRadiusLimit = 4;
+  
+
+  //camera-2
+  const alpha = -Math.PI / 4;
+  const beta = Math.PI / 3;
+  const radius = 8;
+  const target = new BABYLON.Vector3(0, 0, 0);
+
+   camera2 = new BABYLON.ArcRotateCamera(
+    "Camera",
+    alpha,
+    beta,
+    radius,
+    target,
+    scene
+  );
+
+  camera2.attachControl(canvas, true);
+  camera2.lowerRadiusLimit = 3;
+  camera2.upperRadiusLimit = 10;
+
+    var advancedTexture =
+          BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    var button1 = BABYLON.GUI.Button.CreateSimpleButton("button1", "Move");
+    button1.top = "150px";
+    button1.color = "white";
+    button1.width = 0.2;
+    button1.height = 0.1;
+    button1.fontSize = 20;
+    button1.background = "green";
+    button1.cornerRadius = 40
+    button1.bottom= "50px"
+    button1.onPointerUpObservable.add(function () {
+       scene.activeCamera = camera1
+    });
+    var button2 = BABYLON.GUI.Button.CreateSimpleButton("button2", "Don't move");
+    button2.top = "280px";
+    button2.color = "white";
+    button2.width = 0.2;
+    button2.height = 0.1;
+    button2.fontSize = 20;
+    button2.cornerRadius = 40
+    button2.background = "green";
+    button2.onPointerUpObservable.add(function () {
+        scene.activeCamera = camera2
+    });
+    advancedTexture.addControl(button1);
+    advancedTexture.addControl(button2);
     
-    // Sets the sensitivity of the camera to movement and rotation
-    camera.angularSensibility = 1;
-    camera.moveSensibility = 1;
-    
-    // Attach the camera to the canvas
-    camera.attachControl(canvas, true);
-
-
-
-
     const layer = new BABYLON.Layer("layer", null, scene, true);
     BABYLON.VideoTexture.CreateFromWebCam(
         scene,
@@ -60,12 +104,8 @@ const createScene = async () => {
             maxHeight: canvas.height,
             facingMode: "environment",
         }
-    );
-
-            
-    
-        return scene;
-  
+    );         
+    return scene;
 };
 
 
